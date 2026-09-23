bc93349a98e996f774bb8adb50fabacb3f18fac4
JessicaMP <jessica95.mp@gmail.com>
2026-09-23 15:43:14 -0500

    complete task 3

--- task3.js
+++ task3.js
@@ -1,13 +1,17 @@
 // ¡Escribe tu código debajo de esta línea!
 
 function Mail(asunto, mensaje) {
-  this.asunto = asunto
-  this.mensaje = mensaje
+  this.asunto = asunto;
+  this.mensaje = mensaje;
 
+  this.imprimirCorreo = function () {
+    console.log( `${this.asunto}: ${this.mensaje}`);
+    ;
+  };
 }
 
-const nuevoCorreo = new Mail()
+const nuevoCorreo = new Mail("pizza", "piña");
 
 // ¡Escribe tu código encima de esta línea!
 
-nuevoCorreo.imprimirCorreo()
+nuevoCorreo.imprimirCorreo();
