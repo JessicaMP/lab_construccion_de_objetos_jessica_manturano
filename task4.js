34d7ee26e2ffd9bdb7d57b956905a0ecf3618150
JessicaMP <jessica95.mp@gmail.com>
2026-09-23 15:52:02 -0500

    complete task 4

--- task4.js
+++ task4.js
@@ -1,9 +1,12 @@
 // ¡Escribe tu código debajo de esta línea!
 
-
+function Journey(desde, hasta) {
+  this.inicio = desde;
+  this.fin = hasta;
+}
 
 // ¡Escribe tu código encima de esta línea!
 
-const viaje = new Journey(desde, hasta)
+const viaje = new Journey(process.argv[3], process.argv[4])
 
 console.log("Reservando un taxi desde " + viaje.inicio + " hasta " + viaje.fin + ".")
