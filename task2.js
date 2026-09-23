58cb725555f0bbcc31771d536507ad29b9fcca7e
JessicaMP <jessica95.mp@gmail.com>
2026-09-23 15:51:43 -0500

    fix task 2

--- task2.js
+++ task2.js
@@ -5,7 +5,7 @@ function Mail(asunto, mensaje) {
 
 // ¡Escribe tu código debajo de esta línea!
 
-const nuevoCorreo = new Mail('tomate', 'salsa')
+const nuevoCorreo = new Mail(process.argv[3], process.argv[4])
 
 // ¡Escribe tu código encima de esta línea!
 
